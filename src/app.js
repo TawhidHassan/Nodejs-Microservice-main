8168b583ad45b5c224d50855667c41370615518d
sifat hassan <sifat007.sh7@gmail.com>
2021-05-05 14:32:05 +0600

    connect with rabbitmq

--- src/app.js
+++ src/app.js
@@ -3,14 +3,25 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
 var cors = require("cors");
 var typeorm_1 = require("typeorm");
+var amqp = require("amqplib/callback_api");
 var product_1 = require("./entity/product");
 typeorm_1.createConnection().then(function (db) {
     var productRepository = db.getMongoRepository(product_1.Product);
-    var app = express();
-    app.use(cors({
-        origin: ['http://localhost:3000', 'http://localhost:8080', 'http://localhost:4200']
-    }));
-    app.use(express.json());
-    console.log('Listening to port: 8001');
-    app.listen(8001);
+    amqp.connect('amqps://inwmlcbs:3e-VURKX4cVGWW5OGxMp_lHYio7du_-R@clam.rmq.cloudamqp.com/inwmlcbs', function (error0, connection) {
+        if (error0) {
+            throw error0;
+        }
+        connection.createChannel(function (error1, channel) {
+            if (error1) {
+                throw error1;
+            }
+            var app = express();
+            app.use(cors({
+                origin: ['http://localhost:3000', 'http://localhost:8080', 'http://localhost:4200']
+            }));
+            app.use(express.json());
+            console.log('Listening to port: 8001');
+            app.listen(8001);
+        });
+    });
 });
--- src/app.ts
+++ src/app.ts
@@ -2,19 +2,33 @@ import * as express from 'express'
 import {Request, Response} from 'express'
 import * as cors from 'cors'
 import {createConnection} from "typeorm";
-
+import * as amqp from 'amqplib/callback_api';
 import {Product} from "./entity/product";
 
 createConnection().then(db => {
     const productRepository = db.getMongoRepository(Product)
 
-    const app = express()
 
-    app.use(cors({
-        origin: ['http://localhost:3000', 'http://localhost:8080', 'http://localhost:4200']
-    }))
-    app.use(express.json())
+    amqp.connect('amqps://inwmlcbs:3e-VURKX4cVGWW5OGxMp_lHYio7du_-R@clam.rmq.cloudamqp.com/inwmlcbs', (error0, connection) => {
+        if (error0) {
+            throw error0
+        }
 
-    console.log('Listening to port: 8001')
-    app.listen(8001)
+        connection.createChannel((error1, channel) => {
+            if (error1) {
+                throw error1
+            }
+
+            const app = express()
+
+            app.use(cors({
+                origin: ['http://localhost:3000', 'http://localhost:8080', 'http://localhost:4200']
+            }))
+            app.use(express.json())
+        
+            console.log('Listening to port: 8001')
+            app.listen(8001)
+
+        })
+    })
 });
